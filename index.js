424fda2ea5179f7120deab7cf0360bebc505bca7
Lewis Jones Ndubi <lamarjonesndubi@gmail.com>
2024-07-13 23:03:44 +0300

    adds all

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'someone';
+
+
+function changeLeastFavoriteCustomer() {
+    
+    leastFavoriteCustomer = 'someone else';
+}
+
+
+console.log(customerName); 
+upperCaseCustomerName();
+console.log(customerName); 
+
+setBestCustomer();
+console.log(bestCustomer); 
+overwriteBestCustomer();
+console.log(bestCustomer); 
+
+try {
+    changeLeastFavoriteCustomer(); 
+} catch (error) {
+    console.error("You cannot change a constant variable. Error: ", error.message);
+}
+console.log(leastFavoriteCustomer); 
